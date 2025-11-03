99f0540c687ede70f188c6faadbe620a22794668
Slee Woo <mail@sleewoo.com>
2025-11-03 20:58:24 +0200

    package(devlib): defaults

---
+++ packages/core/devlib/src/defaults.ts
@@ -0,0 +1,11 @@
+export const defaults = {
+  appPrefix: "@",
+  coreDir: "core",
+  libDir: "lib",
+  configDir: "config",
+  apiDir: "api",
+  pagesDir: "pages",
+  apiLibDir: "{api}",
+  pagesLibDir: "{pages}",
+  fetchLibDir: "{fetch}",
+};
