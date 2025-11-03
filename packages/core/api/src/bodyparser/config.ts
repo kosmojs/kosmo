e61d3b819860233e1bde28035e8d104b6fa7da47
Slee Woo <mail@sleewoo.com>
2025-11-03 21:08:25 +0200

    package(api): bodyparser

---
+++ packages/core/api/src/bodyparser/config.ts
@@ -0,0 +1,20 @@
+import type { FormOptions, JsonOptions, RawOptions } from "./types";
+
+export const json: JsonOptions = {
+  limit: 1024 ** 2,
+  trim: ["*"],
+};
+
+export const form: FormOptions = {
+  limit: 1024 ** 2,
+};
+
+export const raw: RawOptions = {
+  limit: 1024 ** 2,
+};
+
+export default {
+  json,
+  form,
+  raw,
+};
---
+++ packages/core/api/src/bodyparser/index.ts
@@ -0,0 +1,129 @@
+import zlib from "node:zlib";
+
+import IncomingForm from "formidable";
+import rawParser from "raw-body";
+
+import type { Middleware } from "@kosmojs/api";
+
+import config from "./config";
+import type {
+  FormOptions,
+  JsonOptions,
+  RawOptions,
+  Trimmer,
+  TrimOption,
+} from "./types";
+
+export * from "./types";
+export { config };
+export default { json, form, raw };
+
+export function json(opts: JsonOptions = {}): Array<Middleware> {
+  return [
+    async (ctx, next) => {
+      const form = IncomingForm({
+        maxFieldsSize: opts.limit || config.json.limit,
+        ...opts,
+      });
+
+      const trimmer = trimmerFactory(opts.trim);
+
+      ctx.request.body = await new Promise((resolve, reject) => {
+        form.parse(ctx.request.req, (err, fields) => {
+          if (err) {
+            return reject(err);
+          }
+
+          resolve(trimmer ? trimmer(fields) : fields);
+        });
+      });
+
+      return next();
+    },
+  ];
+}
+
+export function form(opts: FormOptions = {}): Array<Middleware> {
+  return [
+    async (ctx, next) => {
+      const form = IncomingForm({
+        maxFieldsSize: opts.limit || config.form.limit,
+        maxFileSize: opts.limit || config.form.limit,
+        ...opts,
+      });
+
+      let trimmer = trimmerFactory(opts.trim);
+
+      if (opts.multipart || opts.urlencoded) {
+        trimmer = undefined;
+      }
+
+      ctx.request.body = await new Promise((resolve, reject) => {
+        form.parse(ctx.request.req, (err, fields, files) => {
+          if (err) {
+            return reject(err);
+          }
+
+          resolve({
+            fields: trimmer ? trimmer(fields) : fields,
+            files,
+          });
+        });
+      });
+
+      return next();
+    },
+  ];
+}
+
+export function raw(opts: RawOptions = {}): Array<Middleware> {
+  return [
+    async (ctx, next) => {
+      const { chunkSize, ...rawParserOptions } = { ...config.raw, ...opts };
+
+      const stream = ctx.request.req.pipe(zlib.createUnzip({ chunkSize }));
+      ctx.request.body = await rawParser(stream, rawParserOptions);
+
+      return next();
+    },
+  ];
+}
+
+function trimmerFactory(
+  trimOption: TrimOption | undefined,
+): Trimmer | undefined {
+  if (!Array.isArray(trimOption) || !trimOption.length) {
+    return;
+  }
+
+  const trimableKeys: {
+    [key: string]: boolean;
+  } = trimOption.reduce((m: Record<string, boolean>, k) => {
+    m[k] = true;
+    return m;
+  }, {});
+
+  const trim = (key: string, val: unknown) => {
+    return typeof val === "string"
+      ? trimableKeys[key] || trimableKeys["*"]
+        ? val.trim()
+        : val
+      : val;
+  };
+
+  const reducer = (
+    memo: Record<string, unknown>,
+    [key, val]: [string, unknown],
+  ) => {
+    memo[key] = trim(key, val);
+    return memo;
+  };
+
+  return (payload) =>
+    Object.entries(payload).reduce(
+      reducer,
+      // accumulator is set to payload intentionally, to avoid duplication of big strings.
+      // if using a new object for accumulator then trimmed strings will be duplicated?
+      payload,
+    );
+}
