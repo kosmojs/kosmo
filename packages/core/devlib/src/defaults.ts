9f34abab73929f8a35281516374cab703cda51fe
Slee Woo <mail@sleewoo.com>
2025-11-05 20:51:48 +0200

    refactor(devlib): add ssr defaults

--- packages/core/devlib/src/defaults.ts
+++ packages/core/devlib/src/defaults.ts
@@ -1,11 +1,15 @@
 export const defaults = {
   appPrefix: "@",
+
   coreDir: "core",
   libDir: "lib",
   configDir: "config",
   apiDir: "api",
   pagesDir: "pages",
+  ssrDir: "ssr",
+
   apiLibDir: "{api}",
   pagesLibDir: "{pages}",
   fetchLibDir: "{fetch}",
+  ssrLibDir: "{ssr}",
 };
