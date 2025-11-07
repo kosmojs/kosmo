ec727aa77dcaee7a831aed4ba96347b8b674df1d
Slee Woo <mail@sleewoo.com>
2025-11-07 17:30:59 +0200

    deps: tsuit ➜ tfusion

--- packages/core/devlib/src/types.ts
+++ packages/core/devlib/src/types.ts
@@ -1,6 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 
-import type { ResolvedType } from "tsuit";
+import type { ResolvedType } from "tfusion";
 import type { ResolvedConfig } from "vite";
 
 export type PluginOptions = {
