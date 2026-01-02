e32a502480bf377d2ee2ec8f4973a277a51172e4
Slee Woo <mail@sleewoo.com>
2026-01-02 12:30:36 +0200

    package(generators): new package to allow import generators from single place

---
+++ packages/generators/generators/README.md
@@ -0,0 +1,17 @@
+# @kosmojs/generators
+
+TODO: Add package description
+
+## Installation
+
+```sh
+npm install -D @kosmojs/generators
+```
+
+## Documentation
+
+See [kosmojs.dev](https://kosmojs.dev/generators/writing-generators/intro.html)
+
+## License
+
+MIT
---
+++ packages/generators/generators/src/index.ts
@@ -0,0 +1,8 @@
+export { default as apiGenerator } from "@kosmojs/api-generator";
+export { default as fetchGenerator } from "@kosmojs/fetch-generator";
+export { default as openapiGenerator } from "@kosmojs/openapi-generator";
+export { default as reactGenerator } from "@kosmojs/react-generator";
+export { default as solidGenerator } from "@kosmojs/solid-generator";
+export { default as ssrGenerator } from "@kosmojs/ssr-generator";
+export { default as typeboxGenerator } from "@kosmojs/typebox-generator";
+export { default as vueGenerator } from "@kosmojs/vue-generator";
