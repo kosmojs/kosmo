32f792fe7d7539260962d35b66988de3ec6ab2f5
Slee Woo <mail@sleewoo.com>
2026-03-06 20:27:15 +0200

    refine(typebox-generator): add more custom types

--- packages/generators/typebox-generator/src/templates/custom-types.ts
+++ packages/generators/typebox-generator/src/templates/custom-types.ts
@@ -12,6 +12,18 @@ export class TDate extends Type.Base<Date> {
   }
 }
 
+export class TFile extends Type.Base<File> {
+  public override Check(value: unknown): value is File {
+    return value instanceof File;
+  }
+  public override Errors(value: unknown): object[] {
+    return this.Check(value) ? [] : [{ message: "must be File" }];
+  }
+  public override Clone(): TFile {
+    return new TFile();
+  }
+}
+
 export class TBlob extends Type.Base<Blob> {
   public override Check(value: unknown): value is Blob {
     return value instanceof Blob;
@@ -21,6 +33,18 @@ export class TBlob extends Type.Base<Blob> {
   }
   public override Clone(): TBlob {
     return new TBlob();
+  }
+}
+
+export class TBuffer extends Type.Base<Buffer> {
+  public override Check(value: unknown): value is Buffer {
+    return value instanceof Buffer;
+  }
+  public override Errors(value: unknown): object[] {
+    return this.Check(value) ? [] : [{ message: "must be Buffer" }];
+  }
+  public override Clone(): TBuffer {
+    return new TBuffer();
   }
 }
 
@@ -38,6 +62,8 @@ export class TArrayBuffer extends Type.Base<ArrayBuffer> {
 
 export default {
   Date: new TDate(),
+  File: new TFile(),
   Blob: new TBlob(),
+  Buffer: new TBuffer(),
   ArrayBuffer: new TArrayBuffer(),
 };
