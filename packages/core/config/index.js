717c47c9f652c68256f800cf2bd02230c90917c4
Slee Woo <mail@sleewoo.com>
2026-03-27 21:32:38 +0200

    revamp(config): explicitly include lib .d.ts files

--- packages/core/config/index.js
+++ packages/core/config/index.js
@@ -1,6 +1,6 @@
 import { resolve } from "node:path";
 
-import { createTsconfigPaths, renderToFile } from "@kosmojs/lib";
+import { createTsconfigPaths, defaults, renderToFile } from "@kosmojs/lib";
 
 await renderToFile(
   resolve(import.meta.dirname, "tsconfig.vite.json"),
@@ -31,6 +31,11 @@ for (const [framework, jsxImportSource] of [
     JSON.stringify(
       {
         extends: "./tsconfig.vite.json",
+        include: [
+          "${configDir}",
+          `\${configDir}/../../${defaults.libDir}/${defaults.srcDir}/env.d.ts`,
+          `\${configDir}/../../${defaults.libDir}/${defaults.srcDir}/${framework}.d.ts`,
+        ],
         compilerOptions: {
           jsxImportSource,
           paths: createTsconfigPaths("${configDir}/../.."),
