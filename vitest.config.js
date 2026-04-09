4cf412559ade9bbae84c894389cf198966d76681
Slee Woo <mail@sleewoo.com>
2026-04-09 20:02:35 +0300

    chore: revamp vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -10,12 +10,13 @@ const setupFactory = (name, { alias, ...setup } = {}) => {
     extends: true,
     test: {
       name,
+      root: resolve(import.meta.dirname, `packages/${name}`),
+      include: ["test/**/*.test.ts"],
       hookTimeout: 90_000,
-      include: [`packages/${name}/test/**/*.test.ts`],
       alias: {
         ...alias,
-        "@src": resolve(import.meta.dirname, `packages/${name}/src`),
-        "@test": resolve(import.meta.dirname, `packages/${name}/test`),
+        "@src": "src",
+        "@test": "test",
       },
       ...setup,
     },
@@ -27,84 +28,61 @@ export default defineConfig({
   test: {
     projects: [
       setupFactory("core/core", {
-        setupFiles: ["packages/core/core/test/setup.ts"],
+        setupFiles: ["test/setup.ts"],
         globals: true,
       }),
 
       setupFactory("core/cli", {
-        setupFiles: ["packages/core/cli/test/setup.ts"],
+        setupFiles: ["test/setup.ts"],
       }),
 
       setupFactory("core/lib"),
 
       setupFactory("generators/fetch-generator", {
         alias: {
-          [defaults.appPrefix]: resolve(
-            import.meta.dirname,
-            "packages/generators/fetch-generator/test/@fixtures/app",
-          ),
-          [defaults.srcPrefix]: resolve(
-            import.meta.dirname,
-            "packages/generators/fetch-generator/test/@fixtures/app/src",
-          ),
-          [defaults.libPrefix]: resolve(
-            import.meta.dirname,
-            "packages/generators/fetch-generator/test/@fixtures/app/lib/src",
-          ),
+          [defaults.appPrefix]: "test/@fixtures/app",
+          [defaults.srcPrefix]: "test/@fixtures/app/src",
+          [defaults.libPrefix]: "test/@fixtures/app/lib/src",
         },
-        globalSetup: [
-          "packages/generators/fetch-generator/test/setup.global.ts",
-        ],
-        setupFiles: ["packages/generators/fetch-generator/test/setup.ts"],
+        globalSetup: ["test/setup.global.ts"],
+        setupFiles: ["test/setup.ts"],
         globals: true,
       }),
 
       setupFactory("generators/koa-generator", {
-        setupFiles: ["packages/generators/koa-generator/test/setup.ts"],
+        setupFiles: ["test/setup.ts"],
       }),
 
       setupFactory("generators/hono-generator", {
-        setupFiles: ["packages/generators/hono-generator/test/setup.ts"],
+        setupFiles: ["test/setup.ts"],
       }),
 
       setupFactory("generators/openapi-generator", {
         testTimeout: 30_000,
-        globalSetup: [
-          "packages/generators/openapi-generator/test/setup.global.ts",
-        ],
+        globalSetup: ["test/setup.global.ts"],
       }),
 
       setupFactory("generators/typebox-generator", {
-        globalSetup: [
-          "packages/generators/typebox-generator/test/setup.global.ts",
-        ],
+        globalSetup: ["test/setup.global.ts"],
         alias: {
-          [defaults.appPrefix]: resolve(
-            import.meta.dirname,
-            "packages/generators/typebox-generator/test/@fixtures/app",
-          ),
-          [defaults.srcPrefix]: resolve(
-            import.meta.dirname,
-            "packages/generators/typebox-generator/test/@fixtures/app/src",
-          ),
-          [defaults.libPrefix]: resolve(
-            import.meta.dirname,
-            "packages/generators/typebox-generator/test/@fixtures/app/lib/src",
-          ),
+          [defaults.appPrefix]: "test/@fixtures/app",
+          [defaults.srcPrefix]: "test/@fixtures/app/src",
+          [defaults.libPrefix]: "test/@fixtures/app/lib/src",
         },
       }),
 
       setupFactory("generators/solid-generator"),
       setupFactory("generators/react-generator"),
       setupFactory("generators/vue-generator"),
-      setupFactory("generators/ssr-generator"),
 
       setupFactory("integration:api", {
+        root: ".",
         include: ["test/integration/{koa,hono}/*.test.ts"],
       }),
 
       setupFactory("integration:csr", {
-        include: ["test/integration/{react,solid,vue}/*.test.ts"],
+        root: ".",
+        include: ["test/integration/{react,solid,vue,mdx}/*.test.ts"],
         fileParallelism: false,
         provide: {
           CSR: "true",
@@ -112,7 +90,8 @@ export default defineConfig({
       }),
 
       setupFactory("integration:ssr", {
-        include: ["test/integration/{react,solid,vue}/*.test.ts"],
+        root: ".",
+        include: ["test/integration/{react,solid,vue,mdx}/*.test.ts"],
         provide: {
           SSR: "true",
         },
