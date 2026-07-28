2b8706e04b06d81b6944f9a9180b7f522a644727
Slee Woo <mail@sleewoo.com>
2026-07-28 19:58:49 +0300

    .pnpmfile.mjs

---
+++ .pnpmfile.mjs
@@ -0,0 +1,12 @@
+export const hooks = {
+  beforePacking(pkg) {
+    if (process.env.NODE_ENV === "test") {
+      for (const name of Object.keys({ ...pkg.dependencies })) {
+        if (name.includes("kosmo")) {
+          pkg.dependencies[name] = "workspace:^";
+        }
+      }
+    }
+    return pkg;
+  },
+}
