d4fdb958ec104371b015a9caa0e8703056261cde
Slee Woo <mail@sleewoo.com>
2026-07-30 23:11:06 +0300

    fix .pnpmfile.mjs

--- .pnpmfile.mjs
+++ .pnpmfile.mjs
@@ -1,12 +1,14 @@
 export const hooks = {
   beforePacking(pkg) {
     if (process.env.NODE_ENV === "test") {
-      for (const name of Object.keys({ ...pkg.dependencies })) {
-        if (name.includes("kosmo")) {
-          pkg.dependencies[name] = "workspace:^";
+      for (const key of ["dependencies", "devDependencies"]) {
+        for (const name of Object.keys({ ...pkg[key] })) {
+          if (name.includes("kosmo")) {
+            pkg[key][name] = "workspace:^";
+          }
         }
       }
     }
     return pkg;
   },
-}
+};
