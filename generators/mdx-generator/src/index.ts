86236dc4d02dada6a26c6aeb9134eec87486fd55
Slee Woo <mail@sleewoo.com>
2026-08-24 15:35:39 +0300

    fix(mdx-generator): add jsx:preserve

--- generators/mdx-generator/src/index.ts
+++ generators/mdx-generator/src/index.ts
@@ -7,6 +7,7 @@ import type { Options } from "./types";
 export default defineGenerator<Options>({
   meta: {
     name: "MDX",
+    jsx: "preserve",
     jsxImportSource: "preact",
   },
   dependencies: {
