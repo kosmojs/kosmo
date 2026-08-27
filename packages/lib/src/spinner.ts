5f37188b22d1f34402e08328a936325b6c296e86
Slee Woo <mail@sleewoo.com>
2026-08-27 15:03:02 +0300

    revamp(spinnerFactory): return a stub for non-TTY / CI runners

--- packages/lib/src/spinner.ts
+++ packages/lib/src/spinner.ts
@@ -8,8 +8,20 @@ export type SpinnerFactory = {
 };
 
 export const spinnerFactory = (startText: string): SpinnerFactory => {
+  if (!process.stdout.isTTY || process.env.CI || process.env.TERM === "dumb") {
+    // stub for non-TTY / CI runners
+    return {
+      text() {},
+      append() {},
+      succeed() {},
+      failed() {},
+    };
+  }
+
   const spinner = new Spinner().start(startText);
+
   let _text = startText;
+
   return {
     text(text) {
       _text = text;
@@ -34,9 +46,3 @@ export const spinnerFactory = (startText: string): SpinnerFactory => {
     },
   };
 };
-
-export const withSpinner = (
-  text: string,
-  pipe: (spinner: SpinnerFactory) => void | Promise<void>,
-  spinner?: SpinnerFactory,
-) => pipe(spinner || spinnerFactory(text));
